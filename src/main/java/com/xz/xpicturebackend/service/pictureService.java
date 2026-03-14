e0540ab09133d4aa081a04f2fe9611c2bdd4f179
许子星 <14504524+a763314313@user.noreply.gitee.com>
2026-03-14 21:34:29 +0800

    picture

---
+++ src/main/java/com/xz/xpicturebackend/service/pictureService.java
@@ -0,0 +1,13 @@
+package com.xz.xpicturebackend.service;
+
+import com.xz.xpicturebackend.model.entity.picture;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author Administrator
+* @description 针对表【picture(图片)】的数据库操作Service
+* @createDate 2026-03-14 21:30:57
+*/
+public interface pictureService extends IService<picture> {
+
+}
